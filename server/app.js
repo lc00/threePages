4d7247b0b6f2ebee61e08710acdcf37da2632cd2
lc00 <leannecheung00@gmail.com>
2015-06-26 20:53:59 -0600

    set up api routes for getToDoList and add ToDo item and they work

--- server/app.js
+++ server/app.js
@@ -1,19 +1,33 @@
 var express = require('express');
-var webRoutes = require('./web/web-routes');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var webRoutes = require('./web/web');
 var log1 = require('./middleware/log1');
-var apiRoutes = require('./api/api-routes');
+var toDoRoutes = require('./api/routes/toDo');
 // var morgan = require('morgan')
 
 var app = express();
 
+mongoose.connect('mongodb://localhost/toDo');
+
+// bodyParser.json(option) returns middleware that only parses json 
+app.use(bodyParser.json());
+
+//bodyParser.urlencoded(option) returns middleware that only
+//parses the urlencoded bodies
+// a new body object containing the parsed data is populated 
+//on the request object after the middleware (i.e. req.body). 
+//This object will contain key-value pairs, where the value can 
+//be a string or array (when extended is false), or any type 
+//(when extended is true).
+app.use(bodyParser.urlencoded({extended: true}));
+
 log1(app);
-// app.use(morgan('dev'));
 
+toDoRoutes(app);
 webRoutes(app);
-apiRoutes(app);
 
-
-// process is	the window(global object) for node
+// process is window(global object) for node
 var port = process.env.PORT;
 app.listen(port, function(){
 	console.log('this app is listening at port number ' + port);
